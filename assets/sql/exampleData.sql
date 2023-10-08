947231198720ad37f183becf34386dd65cf95d9a
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-10-08 20:08:26 +0200

    Add exampleData.sql

---
+++ assets/sql/exampleData.sql
@@ -0,0 +1,130 @@
+INSERT INTO users(username)
+VALUES ('dang'),
+('tren'),
+('ianstallings'),
+('Animats'),
+('zupa-hu'),
+('spookthesunset'),
+('sieabahlpark'),
+('gman99'),
+('ncr100'),
+('gamblor956'),
+('williswee'),
+('avand'),
+('Mike_Andreuzza'),
+('alonisser'),
+('lamplightdev'),
+('firedup'),
+('evo_9'),
+('dash2'),
+('motiwari'),
+('ryanelkins'),
+('djkn0x'),
+('freezedance'),
+('d8niel'),
+('romaindivuolo'),
+('Peroni'),
+('nodesocket'),
+('daniel-cussen'),
+('alaskamiller'),
+('DanielBMarkham')
+;
+
+INSERT INTO comments(id, text, by_id, time)
+VALUES (21444735, 'Could you please not unsubstantive comments here?<p><a href="https:&#x2F;&#x2F;news.ycombinator.com&#x2F;newsguidelines.html" rel="nofollow">https:&#x2F;&#x2F;news.ycombinator.com&#x2F;newsguidelines.html</a>
+', 1, '2019-11-04 19:31:50'),
+(27115623, 'Can someone explain to me what I&#x27;m missing with regards to how much they&#x27;re currently earning? Looking at their base support sponsors (of which there are 4), they are each paying a minimum of $24,000 each ($96,000&#x2F;year). There are 5 gold sponsors of at least $1000&#x2F;month ($60,000&#x2F;year) and 24 silver sponsors of at least $500&#x2F;month ($120,000&#x2F;year). That&#x27;s already $276K without including the bronze&#x2F;backer tiers.
+', 2, '2021-05-11 10:27:00'),
+(5456802, 'HTML5 and all the surrounding technology is very exciting IMHO. They have the best tools and are quicker to adapt new techniques because of they get to work with a net (the fact that they can rollback quickly if needed). I get a little jealous when I see how quickly they can get something up and running that would take me a few days.
+', 3, '2013-03-28 19:41:48'),
+(12388150, 'The title needs work. The original title &quot;Another Classic Psychology Finding—That You Can Smile Your Way to Happiness—Just Blew Up&quot; is more appropriate.<p>If you can&#x27;t replicate results, it&#x27;s not science.  Deal with it.
+', 4, '2016-08-30 09:29:36'),
+(26526941, 'Super interesting! May I ping you about the results in a few months? Best of luck!
+', 5, '2021-03-21 00:45:28'),
+(32839595, 'Is that as a 1099 or w2?
+', 6, '2022-09-14 18:34:27'),
+(35353801, '[dead]
+]', 7, '2023-03-29 09:27:37'),
+(5893191, 'As someone who went to Uni in Durham and loves the countryside; (but still loved his two year stay in London), I can give you another reason: The sheer variety in cheap cultural things to do. I&#x27;m not saying Newcastle lacks decent culture, just that it&#x27;s not cheap.<p>When living in London, I used to make frequent trips to The Globe (£7 tickets), National Theatre (£12), Sadler&#x27;s Wells (£15), Museums (free) plus other great Opera&#x27;s, plays, contemporary dance, etc all over the city, all for around £15 (usually a <i>lot</i> less) or free.<p>I&#x27;ve since moved out of London as I started missing the countryside, but don&#x27;t be so quick to dismiss everyone who does not make the same choices as you do. The grime, overcrowding, noise &amp; smoke of the city is more than compensated for by the amazing public transport and world class cultural events (again, for &lt;£15 -- Newcastle has great cultural venues but even if they are of the same standard I could not afford to go regularly as it was more than twice as expensive)<p>I would never have discovered my love for Classical music, Ballet or Opera if the opportunities weren&#x27;t so numerous and the tickets so cheap.
+', 8, '2013-06-17 16:07:32'),
+(14895557, '<a href="https:&#x2F;&#x2F;qmlweb.github.io&#x2F;" rel="nofollow">https:&#x2F;&#x2F;qmlweb.github.io&#x2F;</a> (early) demonstrates QML in a browser.<p><a href="https:&#x2F;&#x2F;github.com&#x2F;qmlweb&#x2F;qmlweb&#x2F;projects&#x2F;1" rel="nofollow">https:&#x2F;&#x2F;github.com&#x2F;qmlweb&#x2F;qmlweb&#x2F;projects&#x2F;1</a> shows how much is yet to be done.
+', 9, '2017-07-31 22:19:50'),
+(24970980, 'You really should talk to a lawyer because it sounds like the entire basis for your protest is grounded in a misunderstanding of how the DMCA provisions work.<p>I can tell you based on what you&#x27;ve posted in this thread that you absolutely do not have a tortious interference claim unless you&#x27;ve entered into contracts you haven&#x27;t previously mentioned.',
+10, '2020-11-02 19:00:50')
+;
+
+INSERT INTO stories(id, title, by_id, descendants, score, time, url)
+VALUES (27685842, 'Australian rocket firm Gilmour Space lands $46m in funding
+', 11, NULL, 1, '2021-06-30 09:18:15', 'https://www.techinasia.com/australian-rocket-firm-gilmour-space-lands-46m-funding'),
+(11774035, 'Your bookshelf, where books go to die
+', 12, 0, 2, '2016-05-26 01:36:05', 'https://medium.com/@avand/your-bookshelf-where-books-go-to-die-caa361163656#.g5178j33l'),
+(20809006, 'Show HN: Curated library of and and fonts for digital designers and developers
+', 13, 0, 1, '2019-08-27 14:53:15', 'https://www.colorsandfonts.com/index.html'),
+(21558477, 'New post on scheduling stuff in our software, some patterns and anti patterns
+', 14, 0, 2, '2019-11-17 17:03:21', 'https://itnext.io/on-schedule-2ce0eb21c0b8?source=friends_link&sk=e356e41506160932c99d7d22031cce6f'),
+(23101807, 'Show HN: RemindMe – A PWA Showcasing Notification Triggers
+', 15, 1, 2, '2020-05-07 13:28:16', 'https://remindme.lamplightdev.com/'),
+(23151120, 'DataGene: Python Data Similarity
+', 16, 0, 2, '2020-05-12 08:39:40', 'https://github.com/firmai/datagene'),
+(12783670, 'FLUIDSTANCE L Balance and Motion Redefine the Workplace
+', 17, 0, 1, '2016-10-25 01:00:44', 'https://fluidstance.com/') ,
+(31606567, 'Effective Altruism, Utilitarianism and Localness
+', 18, 0, 2, '2022-06-03 12:36:59', 'https://wyclif.substack.com/p/on-effective-altruism-utilitarianism'),
+(35362384, 'Show HN: Want something better than k-means? Try BanditPAM
+', 19, 5, 7, '2023-03-29 21:50:09', 'https://github.com/motiwari/BanditPAM'),
+(1471646, 'Source of the famous “Now you have two problems” quote (2006)
+', 20, 0, 1, '2010-06-29 17:15:04', 'http://regex.info/blog/2006-09-15/247')
+;
+
+INSERT INTO jobs(id, title, text, by_id, score, time, url)
+VALUES (6516615, 'Science Exchange (YC S11) is looking for a marketing/growth engineer
+', NULL, 21, 1, '2013-10-08 19:55:39', 'https://www.scienceexchange.com/jobs/growth-hacker-growth-engineer'),
+(35620078, 'Toko (YC W22) Is Hiring a Founding Engineer to Teach English with AI
+', 'Toko (YC W22) is on a mission to make English fluency accessible to the world&#x27;s 1.5B English learners. With our app, learners have daily conversations with AI to build confidence for life at work and abroad.<p>We are in the top 3 apps for education in Taiwan, with &gt; 3k paying subscribers (and growing fast)! Many users have reported landing new jobs and confidently moving overseas since using Toko.<p>Founding team (currently 3) has 10+ years of ed-tech experience at MIT, Quizlet, Codecademy (+ experience at Google, Palantir, and Stripe).<p>As a Founding Engineer, you’ll have an outsized equity stake and impact on the product, culture &amp; future of language learning.<p>More details: <a href="https:&#x2F;&#x2F;www.ycombinator.com&#x2F;companies&#x2F;toko&#x2F;jobs&#x2F;hXNmmpi-founding-engineer" rel="nofollow">https:&#x2F;&#x2F;www.ycombinator.com&#x2F;companies&#x2F;toko&#x2F;jobs&#x2F;hXNmmpi-foun...</a><p>Get in touch: jen[at]tokotutor.com
+', 22, 1, '2023-04-18 23:00:20', NULL),
+(6158647, 'iOS Developers Needed to Fix Healthcare
+', NULL, 23, 1, '2013-08-05 07:48:37', 'https://drchrono.com/jobs'),
+(9505846, 'Chariot (YC W15) Is Hiring Mid/Senior Python Engineers and Front-End, Full-Stack
+', NULL, 24, 1, '2015-05-07 17:41:12', 'https://angel.co/chariot/jobs'),
+(35371293, NULL, NULL, NULL, NULL, '2023-03-30 14:00:46', NULL)
+;
+
+INSERT INTO polls(id, title, text, by_id, descendants, score, time)
+VALUES (6262496, 'Poll: Do you personally check references for developers you want to hire?',
+'Here in the UK I&#x27;ve noticed a significant apathy towards checking references. There&#x27;s almost an unwritten rule that says they are pointless.<p>I&#x27;m curious as to what HN&#x27;s take on references are regardless of what country you work in. Do they serve a purpose given the emphasis placed on a developers code etc?<p>Are they the domain of HR as a pure compliance, box ticking exercise? Vote on the poll and share your thoughts.',
+25, 0, 1, '2013-08-23 11:29:22'),
+(14712514, 'Poll: What is your primary job role?',
+NULL, 26, 3, 1, '2017-07-06 20:13:26')
+;
+
+/*
+INSERT INTO pollopts(id, text, by_id, poll, score, time)
+VALUES (917225, 'Some High School', 27, 917219, 17, '2009-11-02 18:09:06'),
+(127228, 'w', 28, 127226, 0, '2008-03-02 07:48:45'),
+(800169, 'Age: 36-50, 4-6 portions a day', 29, 800158, 14, '2009-09-02 16:03:15')
+;
+*/
+
+INSERT INTO parents(item, parent, type)
+VALUES (21444735, 21443986, 'comment'),
+(27115623, 27114718, 'comment'),
+(5456802, 5455508, 'comment'),
+(12388150, 12385734, 'comment'),
+(26526941, 26393273, 'comment'),
+(32839595, 32838798, 'comment'),
+(35353801, 35343919, 'comment'),
+(5893191, 5892744, 'comment'),
+(14895557, 14895327, 'comment'),
+(24970980, 24968306, 'comment')
+;
+
+INSERT INTO deleted(item)
+VALUES (35371293)
+;
+
+INSERT INTO dead(item)
+VALUES (27685842),
+(35353801)
+;
+
+
