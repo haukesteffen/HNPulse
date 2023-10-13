c9fc5a5d183c9429c80c3cc46eab519ea8808b83
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2023-10-13 18:14:56 +0200

    Change username handling, add skipped table

--- assets/sql/createTables.sql
+++ assets/sql/createTables.sql
@@ -3,29 +3,20 @@ DROP TABLE IF EXISTS jobs;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS polls;
 DROP TABLE IF EXISTS pollopts;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS parents;
 DROP TABLE IF EXISTS deleted;
 DROP TABLE IF EXISTS dead;
+DROP TABLE IF EXISTS skipped;
 
-CREATE TABLE users (
-    id SERIAL,
-    username VARCHAR(15),
-    --created TIMESTAMP,
-    --karma INTEGER,
-    --about TEXT,
-  	PRIMARY KEY (id)
-);
 
 CREATE TABLE stories (
     id INTEGER,
     title TEXT,
-    by_id INTEGER,
+    by INTEGER,
     descendants INTEGER,
     score INTEGER,
     time TIMESTAMP,
     url TEXT,
-    CONSTRAINT fk_by FOREIGN KEY(by_id) REFERENCES users(id),
   	PRIMARY KEY (id)
 );
 
@@ -33,20 +24,18 @@ CREATE TABLE jobs (
     id INTEGER,
     title TEXT,
     text TEXT,
-    by_id INTEGER,
+    by INTEGER,
     score INTEGER,
     time TIMESTAMP,
     url TEXT,
-    CONSTRAINT fk_by FOREIGN KEY(by_id) REFERENCES users(id),
   	PRIMARY KEY (id)
 );
 
 CREATE TABLE comments (
     id INTEGER,
     text TEXT,
-    by_id INTEGER,
+    by INTEGER,
     time TIMESTAMP,
-    CONSTRAINT fk_by FOREIGN KEY(by_id) REFERENCES users(id),
   	PRIMARY KEY (id)
 );
 
@@ -54,22 +43,20 @@ CREATE TABLE polls (
     id INTEGER,
     title TEXT,
     text TEXT,
-    by_id INTEGER,
+    by INTEGER,
     descendants INTEGER,
     score INTEGER,
     time TIMESTAMP,
-    CONSTRAINT fk_by FOREIGN KEY(by_id) REFERENCES users(id),
   	PRIMARY KEY (id)
 );
 
 CREATE TABLE pollopts (
     id INTEGER,
     text TEXT,
-    by_id INTEGER,
+    by INTEGER,
     poll INTEGER,
     score INTEGER,
     time TIMESTAMP,
-    CONSTRAINT fk_by FOREIGN KEY(by_id) REFERENCES users(id),
     CONSTRAINT fk_poll FOREIGN KEY(poll) REFERENCES polls(id),
   	PRIMARY KEY (id)
 );
@@ -93,4 +80,8 @@ CREATE TABLE dead (
 
 CREATE TABLE scrape (
     last_id INTEGER
+);
+
+CREATE TABLE skipped (
+    item INTEGER
 );
